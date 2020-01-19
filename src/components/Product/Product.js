3f4fbd4f6d6ee64849f2df90e7b9b0d63bc5729d
Christina <christinathao_714@hotmail.com>
2020-01-19 12:40:05 -0500

    small styling

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,13 +3,16 @@ import React from 'react';
 import './Product.scss';
 
 const Product = ({product}) => {
-    const {systitle, tpprixnum, tpthumbnailuri, sysclickableuri} = product;
+    const {systitle, tpprixnum, tppays, tpthumbnailuri, sysclickableuri} = product;
     return (
         <a href={sysclickableuri}>
             <div className='product'>
                 <img src={tpthumbnailuri} alt="" className='image'/>
                 <div className='title'>{systitle}</div>
-                <div className='price'>$ {tpprixnum}</div>
+                <div className='details'>
+                    <div className='country'>{tppays} |</div>
+                    <div className='price'>${tpprixnum}</div>
+                </div>
             </div>
         </a>
     );
